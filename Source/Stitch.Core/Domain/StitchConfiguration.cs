1b0471e7cfad31904eb5b66b1b1cc3f2ad739653
Nathan Palmer <nate@thinktheweb.com>
2011-07-14 17:11:24 -0400

    Created a Stitch.Nancy to host the one static method to enable stitching into the pipeline Added a Stitch.Tests.Nancy to test the implementation

---
+++ Source/Stitch.Core/Domain/StitchConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System.Configuration;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Stitch
+{
+    [XmlRoot("stitch")]
+    public class StitchConfiguration : IStitchConfiguration
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlArray("paths")]
+        [XmlArrayItem("path")]
+        public string[] Paths { get; set; }
+
+        [XmlArray("dependencies")]
+        [XmlArrayItem("file")]
+        public string[] Dependencies { get; set; }
+
+        [XmlElement("identifier")]
+        public string Identifier { get; set; }
+
+        [XmlArray("compilers")]
+        [XmlArrayItem("compiler")]
+        public IStitchConfigurationCompiler[] Compilers { get; set; }
+
+        [XmlArray("files")]
+        [XmlArrayItem("file")]
+        public IStitchConfiguration[] Files { get; set; }
+    }
+
+        
+
+    [XmlRoot("compiler")]
+    public class StitchConfigurationCompiler : IStitchConfigurationCompiler
+    {
+        [XmlAttribute("type")]
+        public string Type { get; set; }
+
+        [XmlAttribute("extension")]
+        public string Extension { get; set; }
+    }
+}
